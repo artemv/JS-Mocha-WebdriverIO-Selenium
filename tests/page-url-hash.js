c349812f6a9bd2c84c13ed1e4475c2ef8b5574a5
Artem Vasiliev <artem.job@gmail.com>
2018-02-01 22:34:43 +0300

    test: successful test
    ..and es6 syntax

--- tests/page-url-hash.js
+++ tests/page-url-hash.js
@@ -1,13 +1,23 @@
-var expect = require('chai').expect;
+const expect = require('chai').expect;
 
-describe('Mocha Spec Sync example', function() {
+describe('urls with hashes', function() {
+    const baseUrl = 'http://backbonejs.org/';
+    const url1 = `${baseUrl}#Getting-started`;
+    const hash2 = '#Model-View-separation';
+    const url2 = `${baseUrl}${hash2}`;
 
-    it("follows link to new page", function() {
-        var url1 = 'http://backbonejs.org/#Getting-started';
-        var url2 = 'http://backbonejs.org/#Model-View-separation';
+    it('hash-only change works via browser.url', function() {
         browser.url(url1);
         expect(browser.getUrl()).to.equal(url1);
         browser.url(url2);
+        expect(browser.getUrl()).to.equal(url2);
+    });
+
+
+    it('hash-only change works via JS', function() {
+        browser.url(url1);
+        expect(browser.getUrl()).to.equal(url1);
+        browser.execute(() => location.hash = hash, hash2);
         expect(browser.getUrl()).to.equal(url2);
     });
 });
