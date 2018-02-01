f6433870e1b5ee3eb6fe69ef0dea1aafcd003942
Artem Vasiliev <artem.job@gmail.com>
2018-02-02 01:08:49 +0300

    chore(ci): diff. test names

--- tests/page-url-hash1.js
+++ tests/page-url-hash1.js
@@ -4,13 +4,11 @@ const baseUrl = 'http://backbonejs.org/';
 const hash2 = '#Model-View-separation';
 const url2 = `${baseUrl}${hash2}`;
 
-describe('urls with hashes', function() {
-    it('hash-only change works via JS', function() {
-        browser.url(baseUrl);
-        expect(browser.getUrl()).to.equal(baseUrl);
-        browser.execute(function (hash) {
-            location.hash = hash;
-        }, hash2);
-        expect(browser.getUrl()).to.equal(url2);
-    });
+it('hash-only change works via JS', function() {
+    browser.url(baseUrl);
+    expect(browser.getUrl()).to.equal(baseUrl);
+    browser.execute(function (hash) {
+        location.hash = hash;
+    }, hash2);
+    expect(browser.getUrl()).to.equal(url2);
 });
--- tests/page-url-hash2.js
+++ tests/page-url-hash2.js
@@ -4,11 +4,9 @@ const baseUrl = 'http://backbonejs.org/';
 const hash2 = '#Model-View-separation';
 const url2 = `${baseUrl}${hash2}`;
 
-describe('urls with hashes', function() {
-    it('hash-only change works via browser.url', function() {
-        browser.url(baseUrl);
-        expect(browser.getUrl()).to.equal(baseUrl);
-        browser.url(url2);
-        expect(browser.getUrl()).to.equal(url2);
-    });
+it('hash-only change works via browser.url', function() {
+    browser.url(baseUrl);
+    expect(browser.getUrl()).to.equal(baseUrl);
+    browser.url(url2);
+    expect(browser.getUrl()).to.equal(url2);
 });
