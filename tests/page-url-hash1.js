f09b93d9b0a3ceb8e2fac7a51608cdbcee501e88
Artem Vasiliev <artem.job@gmail.com>
2018-02-02 01:13:04 +0300

    chore(ci): diff. test names

--- tests/page-url-hash1.js
+++ tests/page-url-hash1.js
@@ -4,11 +4,13 @@ const baseUrl = 'http://backbonejs.org/';
 const hash2 = '#Model-View-separation';
 const url2 = `${baseUrl}${hash2}`;
 
-it('hash-only change works via JS', function() {
-    browser.url(baseUrl);
-    expect(browser.getUrl()).to.equal(baseUrl);
-    browser.execute(function (hash) {
-        location.hash = hash;
-    }, hash2);
-    expect(browser.getUrl()).to.equal(url2);
+describe('hash-only change via JS', function() {
+    it('works', function() {
+        browser.url(baseUrl);
+        expect(browser.getUrl()).to.equal(baseUrl);
+        browser.execute(function (hash) {
+            location.hash = hash;
+        }, hash2);
+        expect(browser.getUrl()).to.equal(url2);
+    });
 });
