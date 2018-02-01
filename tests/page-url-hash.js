dc21e1182d451cfa63f7f09741b4f399dd866ca2
Artem Vasiliev <artem.job@gmail.com>
2018-02-02 00:28:31 +0300

    chore(ci): diff. test groups for better saucelabs test names

--- tests/page-url-hash.js
+++ tests/page-url-hash.js
@@ -1,27 +1,25 @@
 const expect = require('chai').expect;
 
-describe('urls with hashes', function() {
-    const baseUrl = 'http://backbonejs.org/';
-    const url1 = `${baseUrl}#Getting-started`;
-    const hash2 = '#Model-View-separation';
-    const url2 = `${baseUrl}${hash2}`;
+const baseUrl = 'http://backbonejs.org/';
+const url1 = `${baseUrl}#Getting-started`;
+const hash2 = '#Model-View-separation';
+const url2 = `${baseUrl}${hash2}`;
 
-    beforeEach(function() {
-        browser.url('http://google.com');
-    });
-
-    it('hash-only change works via browser.url', function() {
+describe('hash-only change works via browser.url', function() {
+    it('works', function() {
         browser.url(url1);
         expect(browser.getUrl()).to.equal(url1);
         browser.url(url2);
         expect(browser.getUrl()).to.equal(url2);
     });
 
+});
 
-    it('hash-only change works via JS', function() {
+describe('hash-only change works via JS', function() {
+    it('works', function () {
         browser.url(url1);
         expect(browser.getUrl()).to.equal(url1);
-        browser.execute(function(hash) {
+        browser.execute(function (hash) {
             location.hash = hash;
         }, hash2);
         expect(browser.getUrl()).to.equal(url2);
