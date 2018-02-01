05b95e1112c7aa9873c444e0713b17623f9ad17a
Artem Vasiliev <artem.job@gmail.com>
2018-02-02 00:57:10 +0300

    chore(ci): make them demo the issue again
    so, going from '/path1' to '/path2#hash1' worked fine

--- tests/page-url-hash.js
+++ tests/page-url-hash.js
@@ -1,26 +1,25 @@
 const expect = require('chai').expect;
 
 const baseUrl = 'http://backbonejs.org/';
-const url1 = `https://www.wikipedia.org/`;
+const otherPathUrl = `https://www.wikipedia.org/`;
 const hash2 = '#Model-View-separation';
 const url2 = `${baseUrl}${hash2}`;
 
 describe('urls with hashes', function() {
     afterEach(function() {
-        browser.url(url1);
+        browser.url(otherPathUrl);
     });
 
     it('hash-only change works via browser.url', function() {
-        browser.url(url1);
-        expect(browser.getUrl()).to.equal(url1);
+        browser.url(baseUrl);
+        expect(browser.getUrl()).to.equal(baseUrl);
         browser.url(url2);
         expect(browser.getUrl()).to.equal(url2);
     });
 
     it('hash-only change works via JS', function() {
-        browser.url(url1);
-        expect(browser.getUrl()).to.equal(url1);
         browser.url(baseUrl);
+        expect(browser.getUrl()).to.equal(baseUrl);
         browser.execute(function (hash) {
             location.hash = hash;
         }, hash2);
