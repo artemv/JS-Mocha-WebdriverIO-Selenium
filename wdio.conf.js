aa76077ec7f0b274c5a43adc517313f32eb2897e
Artem Vasiliev <artem.job@gmail.com>
2018-02-01 22:53:38 +0300

    chore(ci): try to set chromedriverVersion for Chrome 64

--- wdio.conf.js
+++ wdio.conf.js
@@ -58,7 +58,7 @@ exports.config = {
         //
       {browserName: 'firefox', platform: 'Windows 10', version: '41.0'},
       {browserName: 'chrome', platform: 'OS X 10.10', version: '63.0'},
-      {browserName: 'chrome', platform: 'OS X 10.10', version: '64.0'},
+      {browserName: 'chrome', platform: 'OS X 10.10', version: '64.0', chromedriverVersion: '2.33'},
       {browserName: 'internet explorer', platform: 'Windows 7', version: '10'}
     ],
     //
